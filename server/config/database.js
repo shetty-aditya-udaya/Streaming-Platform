c1e6a9251e0c46322f262519717cce080e1e4f4f
Shetty Aditya Udaya <shettyaditya266@gmail.com>
2026-02-19 15:38:02 +0530

    Fix Vercel pg error: Explicitly load pg dialectModule

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
@@ -9,6 +10,7 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
+        dialectModule: pg,
         dialectOptions: {
             ssl: {
                 require: true,
